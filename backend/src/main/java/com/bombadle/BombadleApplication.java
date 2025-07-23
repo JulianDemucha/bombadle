46e83606ad64b69a743c8ab24e47b6aee1a04253
JulianDemucha <demuchajulian.it@gmail.com>
2025-07-23 23:49:48 +0200

    change: spring.jpa.hibernate.ddl-auto from validate to none reason: unnecessary after implementing flyway

--- backend/src/main/java/com/bombadle/BombadleApplication.java
+++ backend/src/main/java/com/bombadle/BombadleApplication.java
@@ -2,6 +2,7 @@ package com.bombadle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
