2e49539ed32cb4acad93c9897db2dda8460a08b6
JulianDemucha <demuchajulian.it@gmail.com>
2025-07-25 00:57:05 +0200

    feat: create CharacterCardController, update CharacterCardService - update CharacterCardService: add compareCharacterCard (cardMatcher)   and findCharacterCardById method - create CharacterCardController with compareCard method which uses   compareCharacterCard method - fix CharacterCardRepository error (unable to resolve character_card)   by adding bombadle schema

---
+++ backend/src/main/java/com/bombadle/controller/CharacterCardController.java
@@ -0,0 +1,22 @@
+package com.bombadle.controller;
+
+import com.bombadle.dto.CardMatcher;
+import com.bombadle.entity.CharacterCard;
+import com.bombadle.service.CharacterCardService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/character-cards")
+@AllArgsConstructor
+public class CharacterCardController {
+    private final CharacterCardService characterCardService;
+
+    @GetMapping("/compare/{id}")
+    public CardMatcher.FieldMatcher[] compareCard(@PathVariable Long id) {
+        CharacterCard characterCard = characterCardService.findCharacterCardById(id);
+        if (characterCard != null)
+            return characterCardService.compareCharacterCard(characterCard);
+        else return null;
+    }
+}
