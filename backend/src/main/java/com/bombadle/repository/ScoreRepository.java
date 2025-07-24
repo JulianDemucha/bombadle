ae9f0cbbd025f4c85f2f205f3cff26790a366b0a
JulianDemucha <demuchajulian.it@gmail.com>
2025-07-24 14:50:29 +0200

    feat: add ScoreRepository with top 10 and latest score queries

---
+++ backend/src/main/java/com/bombadle/repository/ScoreRepository.java
@@ -0,0 +1,14 @@
+package com.bombadle.repository;
+
+import com.bombadle.entity.Score;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ScoreRepository extends JpaRepository<Score, Integer> {
+    List<Score> findTop10ByOrderByScoreTimestampAsc(); //zwraca pierwsze 10 score
+    Optional<Score> findTopByOrderByScoreTimestampDesc(); //zwraca najnowszy score
+}
