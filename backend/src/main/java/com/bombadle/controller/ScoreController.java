64b14e084641099affc6ac0a847abd89334a028d
JulianDemucha <demuchajulian.it@gmail.com>
2025-07-24 18:10:58 +0200

    update: ScoreController by adding createScore and deleteScore

--- backend/src/main/java/com/bombadle/controller/ScoreController.java
+++ backend/src/main/java/com/bombadle/controller/ScoreController.java
@@ -3,17 +3,29 @@ package com.bombadle.controller;
 import com.bombadle.entity.Score;
 import com.bombadle.service.ScoreService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/scores")
+@RequestMapping("/api/scores")
 @AllArgsConstructor
 public class ScoreController {
     private final ScoreService scoreService;
+
+    @PostMapping
+    public ResponseEntity<Score> createScore(@RequestBody Score score) {
+        Score saved = scoreService.saveScore(score);
+        return ResponseEntity
+                .created(URI.create("/api/scores/" + saved.getId()))
+                .body(saved);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteScore(@RequestBody Score score) {
+        scoreService.deleteScoreById( score.getId() );
+    }
 
 }
 
