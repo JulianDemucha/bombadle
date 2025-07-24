587f81a53b1f1a527a5e4fca8fde0582f18b6f3e
JulianDemucha <demuchajulian.it@gmail.com>
2025-07-24 15:11:57 +0200

    feat: add ScoreService add some logic to ScoreService add List<Score> findAllOrderByScoreTimestampAsc() to ScoreRepository addd Optional<Score> findByPlayerId(Long playerId) to ScoreRepository add Optional<Score> findById(Long id) to ScoreRepository add void deleteById(Long playerId) to ScoreRepository

--- backend/src/main/java/com/bombadle/repository/ScoreRepository.java
+++ backend/src/main/java/com/bombadle/repository/ScoreRepository.java
@@ -11,4 +11,8 @@ import java.util.Optional;
 public interface ScoreRepository extends JpaRepository<Score, Integer> {
     List<Score> findTop10ByOrderByScoreTimestampAsc(); //zwraca pierwsze 10 score
     Optional<Score> findTopByOrderByScoreTimestampDesc(); //zwraca najnowszy score
+    List<Score> findAllOrderByScoreTimestampAsc();
+    Optional<Score> findByPlayerId(Long playerId);
+    Optional<Score> findById(Long id);
+    void deleteById(Long playerId);
 }
