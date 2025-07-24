72f1b01d0a6678ee4391465d084c8f6b91d283f4
JulianDemucha <demuchajulian.it@gmail.com>
2025-07-24 15:18:12 +0200

    feat: add some logic to ScoreService and create ScoreController note: some changes that were'nt added in the previous commit by mistake has been included hereOD

--- backend/src/main/java/com/bombadle/controller/ScoreController.java
+++ backend/src/main/java/com/bombadle/controller/ScoreController.java
@@ -1,4 +1,19 @@
 package com.bombadle.controller;
 
+import com.bombadle.entity.Score;
+import com.bombadle.service.ScoreService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/scores")
+@AllArgsConstructor
 public class ScoreController {
+    private final ScoreService scoreService;
+
 }
+
--- backend/src/main/java/com/bombadle/service/ScoreService.java
+++ backend/src/main/java/com/bombadle/service/ScoreService.java
@@ -1,4 +1,43 @@
 package com.bombadle.service;
 
+import com.bombadle.entity.Score;
+import com.bombadle.repository.ScoreRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class ScoreService {
+
+    private final ScoreRepository repo;
+
+    public Score saveScore(Score score){
+        return repo.save(score);
+    }
+
+    public List<Score> getAllScores(){
+        return repo.findAll();
+    }
+
+    public Optional<Score> getLatestScore(){
+        return repo.findTopByOrderByScoreTimestampDesc();
+    }
+
+    public Optional<Score> getScoreByPlayerId(Long playerId){
+        return repo.findByPlayerId(playerId);
+    }
+
+    public Optional<Score> getScoreById(Long id){
+        return repo.findById(id);
+    }
+    public void deleteScoreById(Long id) {
+        repo.deleteById(id);
+    }
+
+    public List<Score> getAllScoresSortedAsc(){
+        return repo.findAllOrderByScoreTimestampAsc();
+    }
 }
