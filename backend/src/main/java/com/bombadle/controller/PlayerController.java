6b97575518b1d38a57b93bad7ec5777b1a823833
JulianDemucha <demuchajulian.it@gmail.com>
2025-10-20 04:44:21 +0200

    feat(api): add PlayerController + service + repository method for GET /api/players/all  - PlayerRepository: add findAllByOrderByIdAsc()  - PlayerService: add getAllPlayers()  - create PlayerController and add API endpoint GET /api/players/all
    works btw

--- backend/src/main/java/com/bombadle/controller/PlayerController.java
+++ backend/src/main/java/com/bombadle/controller/PlayerController.java
@@ -1,4 +1,26 @@
 package com.bombadle.controller;
 
+import com.bombadle.dto.PlayerDto;
+import com.bombadle.dto.mapper.PlayerMapper;
+import com.bombadle.service.PlayerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/players")
+@AllArgsConstructor
 public class PlayerController {
+    private final PlayerService playerService;
+    private final PlayerMapper playerMapper;
+
+    @GetMapping("/all")
+    public List<PlayerDto> getAllPlayers() {
+        return playerMapper.toDto(playerService.getAllPlayers());
+    }
+
+
 }
--- backend/src/main/java/com/bombadle/service/PlayerService.java
+++ backend/src/main/java/com/bombadle/service/PlayerService.java
@@ -1,12 +1,18 @@
 package com.bombadle.service;
 
+import com.bombadle.entity.Player;
 import com.bombadle.repository.PlayerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class PlayerService {
     private final PlayerRepository playerRepository;
 
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAllByOrderByIdAsc();
+    }
 }
