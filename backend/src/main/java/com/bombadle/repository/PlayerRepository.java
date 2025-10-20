b6a7407f0b1a5eca1506b7a0b103a1643af32fdd
JulianDemucha <demuchajulian.it@gmail.com>
2025-10-20 04:42:22 +0200

    feat(player): add PlayerDto record and PlayerMapper mapper

--- backend/src/main/java/com/bombadle/repository/PlayerRepository.java
+++ backend/src/main/java/com/bombadle/repository/PlayerRepository.java
@@ -1,14 +1,16 @@
 package com.bombadle.repository;
 
 import com.bombadle.entity.Player;
+import com.bombadle.entity.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     Optional<Player> findByLogin(String Login);
-
     Optional<Player> findByEmail(String email);
+    List<Player> findAllByOrderByIdAsc();
 }
