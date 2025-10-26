f63ecd6b6d094484025073c1e1a14c127e99b003
JulianDemucha <demuchajulian.it@gmail.com>
2025-10-26 13:06:28 +0100

    feat(frontend): add React Router DOM and base routes

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,7 @@
+import MainPage from "./MainPage.jsx";
+// import LoginPage from "./LoginPage.jsx";
+
+export const routes = [
+    { path: '/', element: <MainPage/> },
+    // { path: '/Login', element: <LoginPage/> },
+]
