fcbdc4a06e0cab6abce5db6a7c6bac5bf5447e7d
JulianDemucha <demuchajulian.it@gmail.com>
2025-10-26 13:06:28 +0100

    feat(frontend): add React Router DOM and base routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import './MainPage.jsx';
-import MainPage from "./MainPage.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./Header.jsx";
 import './style/login-button.css';
+import {routes} from "./routes.jsx";
 
 function App() {
 
     return (
         <>
-            <Header />
-            <MainPage />
+            <BrowserRouter>
+                <Header />
+
+                <main style={{ minHeight: '80vh' }}>
+                    <Routes>
+                        {routes.map(r => (
+                            <Route key={r.path} path={r.path} element={r.element} />
+                        ))}
+                    </Routes>
+                </main>
+
+            </BrowserRouter>
         </>
     );
 }
