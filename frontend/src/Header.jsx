da2038d2b069880275359396ddc8fe8bf9ab4046
JulianDemucha <demuchajulian.it@gmail.com>
2025-10-27 15:37:24 +0100

    feat: add imgnavbutton into header and loginpage

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style/logo.css';
 import './style/login-button.css';
+import NavImgButton from "./ImgNavButton.jsx";
 
 const handleImageError = (e) => {
     //todo make placeholders for all img / buttons
@@ -9,32 +10,35 @@ const handleImageError = (e) => {
 
 function Header() {
     return (
-        <>
-            <header className="header-container">
-                <img
-                    src="/img/bombadle_logo.png"
-                    alt="logo"
-                    onError={handleImageError}
-                    className="logo logo-desktop"
-                />
+        <header className="header-container">
 
-                <button className="image-button login-desktop" type="button">
-                    <img
-                        src="/img/LoginButton.png"
-                        alt="Zaloguj się"
-                        onError={handleImageError}
-                    />
-                </button>
-
-            </header>
-            <img
-                src="/img/bombadle_logo_mobile.png"
-                alt="logoMobile"
+            {/*     LOGO     */}
+            <NavImgButton
+                to="/"
+                imgSrc="/img/bombadle_logo.png"
+                altText="logo"
+                className="logo logo-desktop"
                 onError={handleImageError}
-                className="logo logo-mobile"
             />
 
-        </>
+            {/*     LOGIN BUTTON     */}
+            <NavImgButton
+                to="/login"
+                imgSrc="/img/LoginButton.png"
+                altText="Zaloguj się"
+                className="image-button login-desktop"
+                onError={handleImageError}
+            />
+
+            {/*     LOGO MOBILE     */}
+            <NavImgButton
+                to="/"
+                imgSrc="/img/bombadle_logo_mobile.png"
+                altText="logoMobile"
+                className="logo logo-mobile"
+                onError={handleImageError}
+            />
+        </header>
     );
 }
 
--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,55 +1,68 @@
-/* komponent bazowany na https://freefrontend.com/css-login-forms/ */
+/* css komponentu bazowany na https://freefrontend.com/css-login-forms/ */
 import './style/login-page.css';
 import './style/GoogleButton.css'
 import './style/logo.css'
 import React from "react";
 import Footer from "./Footer.jsx";
+import NavImgButton from "./ImgNavButton.jsx";
+
+const handleImageError = (e) => {
+    //todo make placeholders for all img / buttons
+    e.target.src = 'https://placehold.co/544x192/9E6B5D/FFFFFF?text=Przycisk&font=sans-serif';
+};
 
 function LoginPage() {
-    return (
-        <>
+    return (<>
 
-            <div className="login-page">
-                <img
-                    src="/img/bombadle_logo.png"
-                    alt="logo"
-                    className="logo logo-desktop"
-                    style={{maxWidth: '500px', marginBottom: '0.5rem'}}
-                />
-                <img
-                    src="/img/bombadle_logo_mobile.png"
-                    alt="logoMobile"
-                    className="logo logo-mobile"
-                />
-                <div className="login-container">
-                    <h1>LOGOWANIE</h1>
+        {/*     LOGO     */}
+        <NavImgButton
+            to="/"
+            imgSrc="/img/bombadle_logo.png"
+            altText="logo"
+            className="logo logo-desktop"
+            onError={handleImageError}
+        />
 
-                    <div className="input-group">
-                        <label htmlFor="email">EMAIL</label>
-                        <input type="email" id="email" placeholder="twoj@email.com"/>
-                    </div>
+        {/*     LOGO MOBILE     */}
+        <NavImgButton
+            to="/"
+            imgSrc="/img/bombadle_logo_mobile.png"
+            altText="logoMobile"
+            className="logo logo-mobile"
+            onError={handleImageError}
+        />
+        <div className="login-page">
 
-                    <div className="input-group">
-                        <label htmlFor="password">HASŁO</label>
-                        <input type="password" id="password" placeholder="••••••••"/>
-                    </div>
 
-                    <button type="submit">ZALOGUJ SIĘ</button>
 
-                    <div className="divider">LUB</div>
+            <div className="login-container">
+                <h1>LOGOWANIE</h1>
 
-                    <button type="button" className="login-with-google-btn">
-                        ZALOGUJ SIĘ PRZEZ GOOGLE
-                    </button>
-
-                    <div className="loginFooter">
-                        Nie masz konta? <a href="#">Zarejestruj się</a>
-                    </div>
+                <div className="input-group">
+                    <label htmlFor="email">EMAIL</label>
+                    <input type="email" id="email" placeholder="twoj@email.com"/>
                 </div>
-                <Footer className="defFooter"/>
+
+                <div className="input-group">
+                    <label htmlFor="password">HASŁO</label>
+                    <input type="password" id="password" placeholder="••••••••"/>
+                </div>
+
+                <button type="submit">ZALOGUJ SIĘ</button>
+
+                <div className="divider">LUB</div>
+
+                <button type="button" className="login-with-google-btn">
+                    ZALOGUJ SIĘ PRZEZ GOOGLE
+                </button>
+
+                <div className="loginFooter">
+                    Nie masz konta? <a href="/register">Zarejestruj się</a>
+                </div>
             </div>
-        </>
-    )
+            <Footer className="defFooter"/>
+        </div>
+    </>)
 }
 
 export default LoginPage;
