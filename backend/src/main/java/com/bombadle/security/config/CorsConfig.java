b3400e506e44b676b0bc4ed529fc1d9a42f8fa5f
JulianDemucha <demuchajulian.it@gmail.com>
2025-10-29 21:32:03 +0100

    feat(config): add global CORS configuration for cross-origin dev

---
+++ backend/src/main/java/com/bombadle/security/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.bombadle.security.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowCredentials(true)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
