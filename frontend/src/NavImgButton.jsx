b00efe21fe19075820385bc53ee525c0fe25da26
JulianDemucha <demuchajulian.it@gmail.com>
2025-11-01 23:21:35 +0100

    feat(frontend): add hideIfAuthenticated parameter in NavImgButton to hide login buttons

--- frontend/src/NavImgButton.jsx
+++ frontend/src/NavImgButton.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {useAuth} from "./auth/UseAuth.jsx";
 
-function NavImgButton({ to, imgSrc, altText, className, handleImageError: onError, style}) {
-  const navigate = useNavigate();
-
-  const handleClick = () => navigate(to);
-
-  return (
-    <button type="button" className={className} onClick={handleClick} onError={onError} style={style}>
-      <img src={imgSrc} alt={altText} onError={(e) => (e.target.style.display = 'none')} />
-    </button>
-  );
+function NavImgButton({ to, imgSrc, altText, className, onError
+                          , style, hideIfAuthenticated }) {
+    const navigate = useNavigate();
+    const {user} = useAuth();
+    const handleClick = () => navigate(to);
+    if (hideIfAuthenticated && user) {
+        return (<></>);
+    } else {
+        return (
+            <button type="button" className={className} onClick={handleClick} style={style}>
+                <img src={imgSrc} alt={altText} onError={onError}/>
+            </button>
+        );
+    }
 }
 
 export default NavImgButton;
