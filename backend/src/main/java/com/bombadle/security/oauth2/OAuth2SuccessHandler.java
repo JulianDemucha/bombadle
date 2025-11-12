a091ca11ac16036fadb8013c2075e6bc67d151a4
JulianDemucha <demuchajulian.it@gmail.com>
2025-11-12 05:40:26 +0100

    feat(oauth2): update OAuth2SuccessHandler to issue JWT and CSRF cookies on success

--- backend/src/main/java/com/bombadle/security/oauth2/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/bombadle/security/oauth2/OAuth2SuccessHandler.java
@@ -2,10 +2,12 @@ package com.bombadle.security.oauth2;
 
 import com.bombadle.entity.Player;
 import com.bombadle.security.jwt.JwtService;
-import jakarta.servlet.ServletException;
+import com.bombadle.service.CsrfCookieService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -18,21 +20,30 @@ import java.util.HashMap;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtService jwtService;
+    private final CsrfCookieService csrfCookieService;
 
     @Override
     public void onAuthenticationSuccess(
             HttpServletRequest request,
             HttpServletResponse response,
             Authentication authentication
-    ) {
+    ) throws IOException {
 
         CustomOAuth2PlayerUser principal = (CustomOAuth2PlayerUser) authentication.getPrincipal();
-
         Player player = principal.getPlayer();
 
         String jwt = jwtService.generateToken(new HashMap<>(), player);
-        response.setHeader("Authorization", "Bearer " + jwt);
+        ResponseCookie jwtCookie = ResponseCookie.from("jwt", jwt)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(60*60) //1h
+                .sameSite("Lax")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());
+        csrfCookieService.ensureCsrfCookie(request, response);
 
-        // response.sendRedirect("http://localhost:5173/oauth2/success?token=" + jwt);
+        response.sendRedirect("http://localhost:5173/oauth2/success");
+
     }
 }
