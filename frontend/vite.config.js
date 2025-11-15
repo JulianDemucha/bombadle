a15d65dc7f3c737b4cd78e08236767aedcfbc47a
JulianDemucha <demuchajulian.it@gmail.com>
2025-11-15 05:36:31 +0100

    chore(frontend): migrate dev server to HTTPS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import fs from 'fs';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-
-    server: {
-      port: 5173, // frontend (React/Vite)
-      proxy: {
-        '/api': {
-          target: 'https://localhost:8443',
-          changeOrigin: true,
-          secure: false, //dev cert
-        },
+  server: {
+    port: 5173,
+    https: {
+      key: fs.readFileSync(resolve(__dirname, 'certs/key.pem')),
+      cert: fs.readFileSync(resolve(__dirname, 'certs/cert.pem')),
+    },
+    proxy: {
+      '/api': {
+        target: 'https://localhost:8443',
+        changeOrigin: true,
+        secure: false,
       },
     },
+  },
 })
